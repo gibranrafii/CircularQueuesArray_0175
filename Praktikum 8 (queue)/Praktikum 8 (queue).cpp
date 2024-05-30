d005ccb2643993869f47eace2d1fadb3306ddea6
gibranrafii <gibranrafi21@gmail.com>
2024-05-30 11:49:26 +0700

    Membuat kondisi if Jika FRONT <= REAR, iterasi dari FRONT hingga REAR

--- Praktikum 8 (queue)/Praktikum 8 (queue).cpp
+++ Praktikum 8 (queue)/Praktikum 8 (queue).cpp
@@ -71,6 +71,14 @@ public:
 
         cout << "\nElements in the queue are...\n";
 
+        // Jika FRONT <= REAR, iterasi dari FRONT hingga REAR
+        if (FRONT_Position <= REAR_Position) {
+            while (FRONT_Position <= REAR) {
+                cout << queue_array[FRONT_Position] << "   ";
+                FRONT_Position++;
+            }
+            cout << endl;
+        }
     }
 };
 
