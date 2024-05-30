d134d28c01cefe6e830353907e71ed6828883e45
gibranrafii <gibranrafi21@gmail.com>
2024-05-30 11:46:09 +0700

    Membuat cout dan membuat kondisi if untuk cek jika antrian hanya memiliki satu elemen

--- Praktikum 8 (queue)/Praktikum 8 (queue).cpp
+++ Praktikum 8 (queue)/Praktikum 8 (queue).cpp
@@ -43,6 +43,13 @@ public:
             cout << "Queue underflow\n";
             return;
         }
+        cout << "\nThe element deleted from queue is: " << queue_array[FRONT] << "\n";
+
+        // Cek jika antrian hanya memiliki satu elemen
+        if (FRONT == REAR) {
+            FRONT = -1;
+            REAR = -1;
+        }
     }
 
 };
